277585cb514a5b09baf717143ab5a448dd1ff0df
Arjan Lamers <arjanl@aardbeitje.nl>
2014-08-13 20:20:55 +0200

    initial commit

---
+++ earth-runner-server/src/nl/first8/devclub/earthrunner/server/StreetView.java
@@ -0,0 +1,37 @@
+package nl.first8.devclub.earthrunner.server;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.velocity.app.Velocity;
+import org.apache.velocity.runtime.RuntimeConstants;
+import org.apache.velocity.runtime.resource.loader.ClasspathResourceLoader;
+
+public class StreetView extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	
+	@Override
+	public void init() throws ServletException {
+		Velocity.setProperty(RuntimeConstants.RESOURCE_LOADER, "classpath");
+		Velocity.setProperty("classpath.resource.loader.class", ClasspathResourceLoader.class.getName());
+		Velocity.init();
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+	
+		Template t = Velocity.getTemplate("streetview.vm");
+		VelocityContext context = new VelocityContext();
+		t.merge(context, resp.getWriter());
+		resp.getWriter().close();
+	}
+
+}
